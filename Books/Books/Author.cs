313b2ffd27016aac7ae2947dec17a1b3a2a1183b
BohdanHarmata <70683703+BohdanHarmata@users.noreply.github.com>
2020-09-17 08:34:15 +0300

    Add files via upload

---
+++ Books/Books/Author.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books
+{
+    class Authors
+    {
+        string _author;
+
+            
+        public string Content
+        {
+            private get
+            {
+                if (_author != null)
+                    return _author;
+                else
+                    return ("Автора нема");
+            }
+            set
+            {
+                _author = value;
+            }
+        }
+
+
+        public void Show()
+        {
+            Console.WriteLine(_author);
+        }
+    }
+}
