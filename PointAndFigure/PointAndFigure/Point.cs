674a22e0401824cf7c27ca37c041139a50fff61d
BohdanHarmata <70683703+BohdanHarmata@users.noreply.github.com>
2020-09-17 09:18:18 +0300

    Add files via upload

---
+++ PointAndFigure/PointAndFigure/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointAndFigure
+{
+class Point
+    {
+        string name;
+        int x, y;
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public Point()
+            : this("", 0, 0) { }
+        public Point(string name, int x, int y)
+        {
+            Console.WriteLine("Введіть букву вершини:");
+            this.name = Console.ReadLine();
+            Console.WriteLine("Введіть X:");
+            this.x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть Y:");
+            this.y = int.Parse(Console.ReadLine());
+        }
+    }
+}
+
+
---
+++ User/User/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace User
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User myUser = new User( "Andro",  "Богдан", "Гармата", "18","16.09.2020");
+            myUser.Show();
+            Console.ReadKey();
+        }
+    }
+}
