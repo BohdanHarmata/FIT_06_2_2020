657d901b3389a5eda064f7e43de23d828a6b3ea3
BohdanHarmata <70683703+BohdanHarmata@users.noreply.github.com>
2020-09-17 08:39:24 +0300

    Add files via upload

---
+++ Invoice/Invoice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Invoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice myInvoice = new Invoice(Convert.ToInt32(Console.ReadLine()), Console.ReadLine(),Console.ReadLine(),Console.ReadLine());
+            myInvoice.Show();
+            Console.ReadKey();
+        }
+    }
+}
