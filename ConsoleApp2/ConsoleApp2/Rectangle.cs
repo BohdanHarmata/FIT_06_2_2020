563149338bd88b71cdb0f97732d961f3dc3b9fbd
BohdanHarmata <70683703+BohdanHarmata@users.noreply.github.com>
2020-09-17 08:33:03 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Rectangle
+    {
+        private double side1 { get; set; }
+        private double side2 { get; set; }
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public double AreaCalculator(double side1, double side2)
+        {
+            double Area = side1 * side2;
+            return Area;
+        }
+        public double PerimeterCalculator(double side1, double side2)
+        {
+            double Perimeter = 2 * side1 + 2 * side2;
+            return Perimeter;
+        }
+        public void RectangleOut()
+        {
+            Console.WriteLine($"Side1 {side1}");
+            Console.WriteLine($"Side2 {side2}");
+            Console.WriteLine($"Area {AreaCalculator(side1,side2)}");
+            Console.WriteLine($"Perimeter {PerimeterCalculator(side1, side2)}");
+        }
+
+
+    }
+}
