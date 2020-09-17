26d11802c2ef11777d6d17706bc90521f76decdf
BohdanHarmata <70683703+BohdanHarmata@users.noreply.github.com>
2020-09-17 08:32:02 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Addres.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Addres
+    {
+        private int index { get; set; }
+        private string country { get; set; }
+        private string city { get; set; } 
+        private string street { get; set; }
+        private int house { get; set; }
+        private int apartament { get; set; }
+        public Addres(int index, string country, string city, string street, int house, int apartament)
+        {
+            this.index = index;
+            this.country = country;
+            this.city = city;
+            this.street = street;
+            this.house = house;
+            this.apartament = apartament;
+        }
+        public void AddresOut()
+        {
+            Console.WriteLine($"Index {index}");
+            Console.WriteLine($"country {country}");
+            Console.WriteLine($"City {city}");
+            Console.WriteLine($"Street {street}");
+            Console.WriteLine($"House {house}");
+            Console.WriteLine($"Apartament {apartament}");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addres myAddres = new Addres(07400,"Ukraine","Browary","Kyevsky",308,65);
+            myAddres.AddresOut();
+            Console.ReadKey();
+        }
+    }
+}
