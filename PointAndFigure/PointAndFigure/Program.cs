1b861788af43767a07bb27ed7da792333624d19e
BohdanHarmata <70683703+BohdanHarmata@users.noreply.github.com>
2020-09-17 08:35:25 +0300

    Add files via upload

---
+++ PointAndFigure/PointAndFigure/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PointAndFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Figure cube = new Figure("Куб");
+                Console.WriteLine(cube.CalculatePerimeter());
+                Console.ReadKey();
+            }
+        }
+    }
+
