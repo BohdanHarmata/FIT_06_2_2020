718ddcca71b72929182bb1ad5ed3886360b59432
BohdanHarmata <70683703+BohdanHarmata@users.noreply.github.com>
2020-09-17 08:36:31 +0300

    Add files via upload

---
+++ User/User/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User
+{
+    class User
+    {
+        private string _login { get; set; }
+        private string _firstname { get; set; }
+        private string _lastname { get; set; }
+        private string _age { get; set; }
+        readonly string _dataofanket;
+        public User(string login, string firstname, string lastname, string age, string dataofanket) 
+        {
+            this._login = login;
+            this._firstname = firstname;
+            this._lastname = lastname;
+            this._age = age;
+            this._dataofanket = dataofanket;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Логін: {_login}");
+            Console.WriteLine($"Ім'я: {_firstname}");
+            Console.WriteLine($"Призвіще: {_lastname}");
+            Console.WriteLine($"Вік: {_age}");
+            Console.WriteLine($"Дата заповнення анкети: {_dataofanket}");
+        }
+    }
+}
